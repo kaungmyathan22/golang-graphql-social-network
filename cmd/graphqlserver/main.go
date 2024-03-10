0d92bf5ce4abb4ee7192635bd173c364cb75ea7d
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-03-10 11:47:12 +0630

    feat(): main func setup

--- cmd/graphqlserver/main.go
+++ cmd/graphqlserver/main.go
@@ -1,1 +1,21 @@
-package graphqlserver
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/kaungmyathan22/golang-graphql-social-network/config"
+	"github.com/kaungmyathan22/golang-graphql-social-network/database"
+)
+
+func main() {
+	ctx := context.Background()
+	config.LoadEnv(".env")
+	conf := config.New()
+	db := database.New(ctx, conf)
+	if err := db.Migrate(); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Server is running.")
+}
